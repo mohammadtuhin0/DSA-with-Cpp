125d5597b6564eec96a6eb5ec9b8709740fc8ca4
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-11-25 19:17:18 +0600

    test

---
+++ PrimeorNotPrime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 14;
+    // cin>>n;
+    bool isPrime = true;
+
+    for(int i = 2; i <= n-1; i++) {
+        if(n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime == true) {
+        cout<<"Prime No\n";
+    } else {
+        cout<<"Non Prime No\n";
+    }
+    return 0;
+}
