fa987ba6b4cf213b1cadbda5596757d349345c4e
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-11-27 20:03:03 +0600

    Pyramid Pattern

--- Patterns/Pyramid_Pattern.cpp
+++ Patterns/Pyramid_Pattern.cpp
@@ -3,9 +3,21 @@ using namespace std;
 int main() {
     int n =4;
     for(int i=0; i<n; i++) {
-        for(int j=0; )
+        // spaces :  n-i-1
+        for(int j=0;j<n-i-1; j++){
+            cout<< " ";
+        }
+
+        // numbers 1 : i+1
+        for(int j = 1; j<= i+1; j++) {
+            cout<< j;
+        }
+
+        // numbers 2 : 
+        for(int j=i; j>0; j--) {
+            cout<< j;
+        }
+        cout<< endl;
     }
-
-
-    return;
+    return 0;
 }
