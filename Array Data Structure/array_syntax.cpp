74f2e87ae43ed78655b8b3d70a7fbe44bad009a2
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-12-04 08:55:11 +0600

    basic code

--- Array Data Structure/array_syntax.cpp
+++ Array Data Structure/array_syntax.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     // int marks[5] = {99, 100, 85, 79, 90};
     int size = 5;
-    int marks[size];
+    int marks[5];
 
     for(int i=0; i<size; i++) {
         cin>> marks[i];
