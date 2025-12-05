912c0896600e27da905f74929def796deb0bea77
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-12-05 10:40:33 +0600

    Linear Search

---
+++ Array Data Structure/Linear_Search.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int sz, int target){
+    for(int i=0; i<sz; i++) {
+        if(arr[i] == target ) {
+            return i;
+        }
+    }
+    return -1;
+}
+int main() {
+    int arr[] = {4, 8, 7, 5, 6};
+    int sz = 5;
+    int target = 8;
+
+    cout<< linearSearch(arr, sz, target) <<endl;
+    return 0;
+}
