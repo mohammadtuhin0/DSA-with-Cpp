eb117f78d9d6f341a1362334471843fcc3086f59
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-12-06 19:57:55 +0600

    vector size

---
+++ Vectors in Arrays/vector_size.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector <char> vec = {'a', 'b', 'c', 'd'};
+
+    cout<< "Size = " << vec.size() <<endl;
+    for(char val : vec){        // for each loop
+        cout<< val << endl;
+    }
+    return 0;
+}
